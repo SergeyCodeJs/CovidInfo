db10bdd4c76badbd2a07c45565b9e72c19bc4aff
SergeyCodeJs <sergeysteh@gmail.com>
2020-03-31 12:33:49 +0800

    feat: add build

---
+++ build/precache-manifest.925624fe219436765cfb29f100e59b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f9cb8607688b259fcc429a4d0f6cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7624802efa8225a83b",
+    "url": "/static/css/main.ab125839.chunk.css"
+  },
+  {
+    "revision": "446b488870999d93d76b",
+    "url": "/static/js/2.4a9b28e3.chunk.js"
+  },
+  {
+    "revision": "729d4e804bb18fd39a884c2e5c10365a",
+    "url": "/static/js/2.4a9b28e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7624802efa8225a83b",
+    "url": "/static/js/main.e86ef9c3.chunk.js"
+  },
+  {
+    "revision": "beac14da8b28c9d9f000",
+    "url": "/static/js/runtime-main.0ce5e76a.js"
+  }
+]);
