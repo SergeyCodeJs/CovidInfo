aa82fb35cbd4daf9f2c97bf327826a72c2db802a
SergeyCodeJs <sergeysteh@gmail.com>
2020-05-03 19:30:28 +0800

    fix: new build version added

---
+++ build/precache-manifest.4fa7eda8de6caf9bedf37c0fc91d8a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7589503e359141fdb886d0aabd3177f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6448d57679273906b8",
+    "url": "/static/css/main.8774cfda.chunk.css"
+  },
+  {
+    "revision": "e683440c343eee6fc6cb",
+    "url": "/static/js/2.1ab0df96.chunk.js"
+  },
+  {
+    "revision": "a25cc1a7735e44380bf097b418b6377c",
+    "url": "/static/js/2.1ab0df96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6448d57679273906b8",
+    "url": "/static/js/main.6abfaf8b.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  }
+]);
